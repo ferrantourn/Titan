9f26a7d414bc9d089c585f5f5a1917f283473ad4
Amalfi Marini <amalfi@capicua.com.uy>
2014-04-20 23:29:35 -0300

    commit pascuas 5

---
+++ TitanDesktop/Resizer.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Titan
+{
+    partial class Resizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResizerBrowser = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ResizerBrowser
+            // 
+            this.ResizerBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResizerBrowser.Location = new System.Drawing.Point(12, 47);
+            this.ResizerBrowser.Name = "ResizerBrowser";
+            this.ResizerBrowser.Size = new System.Drawing.Size(642, 612);
+            this.ResizerBrowser.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.btnAceptar);
+            this.panel1.Controls.Add(this.ResizerBrowser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 662);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(12, 12);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAceptar.Size = new System.Drawing.Size(137, 29);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar y Guardar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // Resizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 662);
+            this.Controls.Add(this.panel1);
+            this.Name = "Resizer";
+            this.Text = "Resizer";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.WebBrowser ResizerBrowser;
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAceptar;
+    }
+}
