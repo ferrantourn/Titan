9b59fdbef5f9cce8010e08167fa8bbf07e0c5282
Amalfi Marini <amalfi@capicua.com.uy>
2014-05-05 19:25:54 -0300

    Titan Web Commit 17

--- TitanWeb/TitanWeb/Persistencia.cs
+++ TitanWeb/TitanWeb/Persistencia.cs
@@ -23,15 +23,53 @@ namespace TitanWeb
             websiteToImage.Generate();
         }
 
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            // If the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location. 
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
         public void GuardarProyectoAutolider(string nombre, AutoliderContainer c)
         {
             try
             {
                 Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/") + nombre);
-                foreach (var file in HttpContext.Current.Server.MapPath("~/UploadedImages"))
-                {
-                    File.Copy(file, Path.Combine(HttpContext.Current.Server.MapPath("~/").ToString() + nombre.ToString(), Path.GetFileName(file)));
-                }
+                //foreach (var file in HttpContext.Current.Server.MapPath("~/UploadedImages"))
+                //{
+                //    File.Copy(file, Path.Combine(HttpContext.Current.Server.MapPath("~/").ToString() + nombre.ToString(), Path.GetFileName(file)));
+                //}
 
                 string sMarca = c.SMarca;
                 string sModelo = c.SModelo;
